9dbbb7dae43c0a446faafed267490ea515dfc86c
Rafael Fadel Silva <rafaelfadel2@gmail.com>
2022-07-25 08:57:32 -0300

    basic game logic completed

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use core::fmt;
-use std::io::stdin;
+use std::{io::stdin, string::ParseError};
 
 struct Player1 {}
 
 struct Player2 {}
 
-#[derive(Debug)]
-enum MoveStates {
+#[derive(Debug, Clone)]
+pub enum MoveStates {
     X,
     O,
-    None,
+    N,
 }
 
 impl fmt::Display for MoveStates {
@@ -18,12 +18,12 @@ impl fmt::Display for MoveStates {
         match *self {
             MoveStates::X => write!(f, "X"),
             MoveStates::O => write!(f, "O"),
-            MoveStates::None => write!(f, "None"),
+            MoveStates::N => write!(f, "N"),
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Game {
     board: Vec<Vec<MoveStates>>,
 }
@@ -32,13 +32,13 @@ impl Game {
     pub fn new() -> Game {
         Game {
             board: vec![
-                vec![MoveStates::None, MoveStates::X, MoveStates::None],
-                vec![MoveStates::None, MoveStates::X, MoveStates::None],
-                vec![MoveStates::None, MoveStates::X, MoveStates::None],
+                vec![MoveStates::N, MoveStates::N, MoveStates::N],
+                vec![MoveStates::N, MoveStates::N, MoveStates::N],
+                vec![MoveStates::N, MoveStates::N, MoveStates::N],
             ],
         }
     }
-    pub fn get_move() -> (u8, u8) {
+    pub fn get_move() -> Result<(usize, usize)> {
         println!("What is your move's X co-ordinate?: ");
         let mut user_cords1 = String::new();
         stdin()
@@ -52,38 +52,46 @@ impl Game {
             .read_line(&mut user_cords2)
             .ok()
             .expect("Failed to read line.");
+
         let coords = (
             user_cords1
-                .parse::<u8>()
-                .ok()
-                .expect("Program only process numbers."),
+                .trim()
+                .parse::<usize>()
+                .context("Failed to parse")?,
             user_cords2
-                .parse::<u8>()
-                .ok()
-                .expect("Program only process numbers."),
+                .trim()
+                .parse::<usize>()
+                .context("Failed to parse")?,
         );
-
         println!("({}, {})", user_cords1.trim(), user_cords2.trim());
-        coords
+        Ok(coords)
     }
 
-    pub fn make_a_move(&mut self, cords: (u8, u8), player: MoveStates) -> Self {}
+    pub fn make_a_move(&mut self, cords: (usize, usize), player: MoveStates) -> &mut Self {
+        let (x, y) = cords;
+        match self.board[x][y] {
+            MoveStates::X => panic!("Illegal move!"),
+            MoveStates::O => panic!("Illegal move!"),
+            MoveStates::N => self.board[x][y] = player,
+        }
+        self
+    }
 
     pub fn render(&self) {
         println!(
-            "     0      1     2\n-----------------------\n1 | {}, | {}, | {} |",
+            "    0    1    2\n-----------------\n0 | {}, | {}, | {} |",
             self.board[0][0], self.board[0][1], self.board[0][2]
         );
-        println!("-----------------------");
+        println!("-----------------");
         println!(
-            "2 | {}, | {}, | {} |",
+            "1 | {}, | {}, | {} |",
             self.board[1][0], self.board[1][1], self.board[1][2]
         );
-        println!("-----------------------");
+        println!("-----------------");
         println!(
-            "3 | {}, | {}, | {} |",
+            "2 | {}, | {}, | {} |",
             self.board[2][0], self.board[2][1], self.board[2][2]
         );
-        println!("-----------------------");
+        println!("-----------------");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-use tic_tac_toer::Game;
-fn main() {
-    let game = Game::new();
+use anyhow::Result;
+use tic_tac_toer::{Game, MoveStates};
+fn main() -> Result<()> {
+    let mut game = Game::new();
     Game::render(&game);
-    Game::get_move();
+
+    let mut rounds = 0;
+    while rounds < 10 {
+        let player1 = MoveStates::X;
+        let player2 = MoveStates::O;
+        let mov = Game::get_move()?;
+
+        Game::make_a_move(&mut game, mov, player1);
+        Game::render(&game);
+
+        let mov = Game::get_move()?;
+        Game::make_a_move(&mut game, mov, player2);
+        Game::render(&game);
+        rounds += 1;
+    }
+
+    Ok(())
 }
